d5b2f2e381e42b396efe946fb233cf33ccf531ce
Norrismi <Norrismi0@gmail.com>
2021-01-15 16:00:51 -0500

    prod map

--- src/Components/listings/listingDetails/listingMap.js
+++ src/Components/listings/listingDetails/listingMap.js
@@ -8,8 +8,9 @@ const ListingMap = (props) => {
 
     const [viewport, setViewport] = useState({
         latitude: props[0],
-        longitude: props[1],
-        zoom: 17,
+        longitude: -89.9109,
+        // zoom: 17,
+        zoom: 5,
         height: '500px',
         width: '100%',
 
@@ -21,12 +22,13 @@ const ListingMap = (props) => {
         <div className='map_container'>
             <ReactMapGL {...viewport}
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+                // mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+                mapStyle='mapbox://styles/norrismi/ckjj9pi7n0ez01as1sk0z1hmo'
                 onViewportChange={viewport => {
                     setViewport(viewport)
                 }}
             >
-                <Marker className='map_marker-container' latitude={props[0]} longitude={props[1]}>
+                <Marker className='map_marker-container' latitude={viewport.latitude} longitude={viewport.longitude}>
                     <img className='map_red-pin' src={red_pin} alt='marker of property' />
                 </Marker>
             </ReactMapGL>
